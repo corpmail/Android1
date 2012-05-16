33f107f461468975d81c24a458239e75a5890aed
Admin <Admin@user1-PC2>
2012-05-16 21:48:15 +0200

    Mm1

---
+++ AndTabs2/src/com/nastev/test/AndTabs2Activity.java
@@ -0,0 +1,13 @@
+package com.nastev.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndTabs2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
