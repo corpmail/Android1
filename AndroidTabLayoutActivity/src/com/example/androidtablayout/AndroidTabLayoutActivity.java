f2d26aca16f3a8c7f337a6d9ff1cb747719e8ac6
Admin <Admin@user1-PC2>
2012-05-16 21:55:28 +0200

    Initial

---
+++ AndroidTabLayoutActivity/src/com/example/androidtablayout/AndroidTabLayoutActivity.java
@@ -0,0 +1,43 @@
+package com.example.androidtablayout;
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+public class AndroidTabLayoutActivity extends TabActivity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+ 
+        TabHost tabHost = getTabHost();
+ 
+        // Tab for Photos
+        TabSpec photospec = tabHost.newTabSpec("Photos");
+        // setting Title and Icon for the Tab
+        photospec.setIndicator("Photos");
+        Intent photosIntent = new Intent(this, PhotosActivity.class);
+        photospec.setContent(photosIntent);
+ 
+        // Tab for Songs
+        TabSpec songspec = tabHost.newTabSpec("Songs");
+        songspec.setIndicator("Songs");
+        Intent songsIntent = new Intent(this, SongsActivity.class);
+        songspec.setContent(songsIntent);
+ 
+        // Tab for Videos
+        TabSpec videospec = tabHost.newTabSpec("Videos");
+        videospec.setIndicator("Videos");
+        Intent videosIntent = new Intent(this, VideosActivity.class);
+        videospec.setContent(videosIntent);
+ 
+        // Adding all TabSpec to TabHost
+        tabHost.addTab(photospec); // Adding photos tab
+        tabHost.addTab(songspec); // Adding songs tab
+        tabHost.addTab(videospec); // Adding videos tab
+    }
+}
